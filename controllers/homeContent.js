dc3930947afc0b68eccaddee61569a3a01beb8be
meriam <hamdaoui.meriam85@gmail.com>
2022-05-17 17:43:14 +0200

    challenge 13: little bit of js inside our home.ejs

--- controllers/homeContent.js
+++ controllers/homeContent.js
@@ -1,4 +1,4 @@
 const homeContent = (req, res, homeStartingContent, posts) => {
-  res.render("home", { homeParagraph: homeStartingContent, posts: posts });
+  res.render("home", { homeParagraph: homeStartingContent, postItems: posts });
 };
 module.exports = { homeContent };
